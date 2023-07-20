a9de4c38107b356148bdee7b4a34b1cd6cd1fc2c
hahuy2kbg <huyhue2kbg@gmail.com>
2023-07-20 10:29:57 +0700

    huyup2007

--- app/Models/Posts.php
+++ app/Models/Posts.php
@@ -5,15 +5,17 @@ namespace App\Models;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class typepost extends Model
+class Posts extends Model
 {
     use HasApiTokens, HasFactory, Notifiable, SoftDeletes;
-
     protected $fillable = [
+        'image',
         'name',
-        'isActive'
+        'title',
+        'content',
+        'type',
     ];
 }
---
+++ database/migrations/2023_07_20_032031_create_table_posts.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTablePosts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements("id");
+            $table->string('image',250);
+            $table->string('name');
+            $table->string('title');
+            $table->string('content');
+            $table->integer('type');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
